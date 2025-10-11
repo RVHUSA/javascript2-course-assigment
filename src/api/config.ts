cd92997de72073722eccbde528c3aa73000c1a78
RVHUSA <rebecca.husa@hotmail.com>
2025-10-11 19:30:15 +0200

    Update config.ts
    Added API key

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,8 @@
 export const API_BASE = "https://v2.api.noroff.dev";
 export const AUTH_URL = `${API_BASE}/auth`;
 export const SOCIAL_URL = `${API_BASE}/social`;
+
+// My API key
+export const API_KEY = "6112549e-9325-4f5b-84b8-4a512399168d";
+
+
