3bb82b31ddaf7c9c75341ff7195ec540f5698d43
RVHUSA <rebecca.husa@hotmail.com>
2025-11-09 21:35:00 +0100

    Fix
    Due to deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Lag dirname fra import.meta.url
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: ".",
-  publicDir: "public",
+  root: "src", 
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
+    outDir: "../dist", 
     rollupOptions: {
       input: {
-        main: "index.html",
-        login: "src/html/login.html",
-        register: "src/html/register.html",
-        post: "src/html/post.html",
-        edit: "src/html/edit.html",
+        feed: resolve(__dirname, "src/html/feed.html"),
+        login: resolve(__dirname, "src/html/login.html"),
+        register: resolve(__dirname, "src/html/register.html"),
+        edit: resolve(__dirname, "src/html/edit.html"),
+        post: resolve(__dirname, "src/html/post.html"),
+        profile: resolve(__dirname, "src/html/profile.html"),
       },
     },
   },
