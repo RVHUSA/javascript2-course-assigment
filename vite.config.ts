acbd16453ad2a3486cb2a98c1df3f5d3c40933a1
RVHUSA <rebecca.husa@hotmail.com>
2025-11-09 22:07:32 +0100

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 import { defineConfig } from "vite";
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
-
-// Lag dirname fra import.meta.url
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import { resolve } from "path";
 
 export default defineConfig({
-  root: "src", 
+  root: "src",       
   build: {
     outDir: "../dist", 
     rollupOptions: {
       input: {
-        feed: resolve(__dirname, "src/html/feed.html"),
         login: resolve(__dirname, "src/html/login.html"),
         register: resolve(__dirname, "src/html/register.html"),
-        edit: resolve(__dirname, "src/html/edit.html"),
-        post: resolve(__dirname, "src/html/post.html"),
+        feed: resolve(__dirname, "src/html/feed.html"),
         profile: resolve(__dirname, "src/html/profile.html"),
+        post: resolve(__dirname, "src/html/post.html"),
+        myPosts: resolve(__dirname, "src/html/myPosts.html"),
+        edit: resolve(__dirname, "src/html/edit.html"),
       },
     },
   },
