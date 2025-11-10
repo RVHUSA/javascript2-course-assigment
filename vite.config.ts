6563c02d74c0a6994eb78736084c2c312c2bef85
RVHUSA <rebecca.husa@hotmail.com>
2025-11-10 18:24:23 +0100

    Created a fake index

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import { copyFileSync } from "fs";
 
 export default defineConfig({
   root: "src",
@@ -9,21 +8,14 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        feed: resolve(__dirname, "src/html/feed.html"),
+        index: resolve(__dirname, "src/index.html"),  
         login: resolve(__dirname, "src/html/login.html"),
         register: resolve(__dirname, "src/html/register.html"),
         profile: resolve(__dirname, "src/html/profile.html"),
         singlePost: resolve(__dirname, "src/html/post.html"),
         edit: resolve(__dirname, "src/html/edit.html"),
+        feed: resolve(__dirname, "src/html/feed.html"), 
       },
     },
   },
-  plugins: [
-    {
-      name: "copy-feed-to-index",
-      closeBundle() {
-                copyFileSync("dist/html/feed.html", "dist/index.html");
-      },
-    },
-  ],
 });
