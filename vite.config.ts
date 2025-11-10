48f5ff7886d54e04e7711985ee0c41ad32b979cc
RVHUSA <rebecca.husa@hotmail.com>
2025-11-10 16:34:01 +0100

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
-  root: "src", 
+  root: "src",
   build: {
-    outDir: "../dist", 
-    rollupOptions: {
-      input: {
-        feed: resolve(__dirname, "public/feed.html"),
-        login: resolve(__dirname, "public/login.html"),
-        register: resolve(__dirname, "public/register.html"),
-        profile: resolve(__dirname, "public/profile.html"),
-        myPosts: resolve(__dirname, "public/post.html"),
-        edit: resolve(__dirname, "public/edit.html"), 
-      },
-    },
+    outDir: "../dist",
+    emptyOutDir: true,
   },
 });
 
