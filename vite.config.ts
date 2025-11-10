ef04cb7cf16a4a1f9f4237851d1547c4491276e2
RVHUSA <rebecca.husa@hotmail.com>
2025-11-10 15:53:12 +0100

    Fix
    Wrong path

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
     outDir: "../dist", 
     rollupOptions: {
       input: {
-        feed: resolve(__dirname, "src/html/feed.html"),
-        login: resolve(__dirname, "src/html/login.html"),
-        register: resolve(__dirname, "src/html/register.html"),
-        profile: resolve(__dirname, "src/html/profile.html"),
-        myPosts: resolve(__dirname, "src/html/post.html"),
-        edit: resolve(__dirname, "src/html/edit.html"), 
+        feed: resolve(__dirname, "public/feed.html"),
+        login: resolve(__dirname, "public/login.html"),
+        register: resolve(__dirname, "public/register.html"),
+        profile: resolve(__dirname, "public/profile.html"),
+        myPosts: resolve(__dirname, "public/post.html"),
+        edit: resolve(__dirname, "public/edit.html"), 
       },
     },
   },
