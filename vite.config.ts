0c401bc0f53b020afa07a4c33ef1eba572f6035a
RVHUSA <rebecca.husa@hotmail.com>
2025-11-10 16:51:56 +0100

    Changed path again

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "src",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        feed: resolve(__dirname, "src/html/feed.html"),
+        login: resolve(__dirname, "src/html/login.html"),
+        register: resolve(__dirname, "src/html/register.html"),
+        profile: resolve(__dirname, "src/html/profile.html"),
+        singlePost: resolve(__dirname, "src/html/post.html"),
+        edit: resolve(__dirname, "src/html/edit.html"),
+      },
+    },
   },
 });
-
-
